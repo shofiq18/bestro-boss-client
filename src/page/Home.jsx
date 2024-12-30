63deea158f0e268a049635c88c12c24f048acc23
shofiq18 <shafiqulislam25021998@gmail.com>
2024-12-30 21:55:56 +0600

    added button

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             <button className="btn btn-info">Info</button>
             <button className="btn btn-success">Success</button>
             <button className="btn btn-warning">Warning</button>
-            <button className="btn btn-error">Error</button>
+          
         </div>
     );
 };
